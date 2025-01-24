d2236eb0314f24ea979a2dea08f34f91e2db8df9
Bhuvanesh24 <bhuvaneshwaran813@gmail.com>
2025-01-24 21:35:56 +0530

    Deployment

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const path = require('path');
 const incomeRoutes = require("./routes/incomeRoutes.js");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
